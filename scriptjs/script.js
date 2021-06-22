41c5b7ded00df8ffab09284ecf9c39ebcc224249
rafaapcode <83471941+rafaapcode@users.noreply.github.com>
2021-06-22 19:37:45 -0300

    Update script.js

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -3,7 +3,7 @@ const criarItem = (text, status) => {
 
     const item = document.createElement("label");
     item.classList.add("todo__item");
-    item.innerHTML = `<input type="checkbox" ${status}> <div>${tarefa}</div> <input type="button" value="X">`;
+    item.innerHTML = `<input type="checkbox" ${status}> <div>${text}</div> <input type="button" value="X">`;
 
     document.getElementById("todoList").appendChild(item);
 
