303e65f83da2bceafe06349aa2b88a09ba763668
rafaapcode <83471941+rafaapcode@users.noreply.github.com>
2021-06-22 17:26:32 -0300

    Criar tarefa

---
+++ scriptjs/script.js
@@ -0,0 +1,10 @@
+// Criar a tarefa
+const criarItem = (text, status) => {
+
+    const item = document.createElement("label");
+    item.classList.add("todo__item");
+    item.innerHTML = `<input type="checkbox" ${status}> <div>${tarefa}</div> <input type="button" value="X">`;
+
+    document.getElementById("todoList").appendChild(item);
+
+}
